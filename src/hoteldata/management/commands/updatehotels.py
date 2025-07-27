e2590b392b5c12aa2f30d7e17595eea26e6045d1
Coen Konings <coen.konings@hotmail.com>
2025-07-27 17:35:42 +0200

    get csv files

--- src/hoteldata/management/commands/updatehotels.py
+++ src/hoteldata/management/commands/updatehotels.py
@@ -1,10 +1,26 @@
+import os
+
+import requests
 from django.core.management.base import BaseCommand
+from dotenv import load_dotenv
 
 
 class Command(BaseCommand):
     help = "Update hotel data"
 
     def handle(self, *args, **options):
-        self.stdout.write(
-            self.style.SUCCESS("Sucessfully did nothing. (TODO: implement)")
-        )
+        load_dotenv()
+        username = os.environ["HOTELDATA_USERNAME"]
+        password = os.environ["HOTELDATA_PASSWORD"]
+        cities_url = os.environ["HOTELDATA_CITIES_URL"]
+        hotels_url = os.environ["HOTELDATA_HOTELS_URL"]
+
+        basic_auth = requests.auth.HTTPBasicAuth(username, password)
+
+        self.stdout.write("Getting cities...")
+        cities_result = requests.get(cities_url, auth=basic_auth)
+        self.stdout.write(cities_result.text)
+
+        self.stdout.write("Getting hotels...")
+        hotels_result = requests.get(hotels_url, auth=basic_auth)
+        self.stdout.write(hotels_result.text)
