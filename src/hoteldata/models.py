7c2f4e6a8b95fb72e5e391ec7e1ba66c4f140076
Coen Konings <coen.konings@hotmail.com>
2025-07-28 09:34:54 +0200

    create city model

--- src/hoteldata/models.py
+++ src/hoteldata/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    # Longest place name in the world has 85 letters.
+    name = models.CharField(max_length=85)
+    # 3 letter identifier. Used to differentiate cities with the same name.
+    abbreviation = models.CharField(max_length=3, unique=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.abbreviation})"
