5b8308077649e81d6215ec79f77bda29e3a693b6
Coen Konings <coen.konings@hotmail.com>
2025-07-28 10:13:42 +0200

    create hotel model

--- src/hoteldata/models.py
+++ src/hoteldata/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 
 class City(models.Model):
-    # Longest place name in the world has 85 letters.
     name = models.CharField(max_length=85)
     # 3 letter identifier. Used to differentiate cities with the same name.
     abbreviation = models.CharField(max_length=3, unique=True)
 
     def __str__(self):
         return f"{self.name} ({self.abbreviation})"
+
+
+class Hotel(models.Model):
+    name = models.CharField()
+    city = models.ForeignKey(City, on_delete=models.PROTECT)
+    index = models.PositiveSmallIntegerField(
+        db_comment="Combines with city abbreviation to create hotel code."
+    )
