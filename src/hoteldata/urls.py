c7de63d8b407ad9231da8182acd5c88ad073befb
Coen Konings <coen.konings@hotmail.com>
2025-07-28 17:15:18 +0200

    filter hotels by city in url

--- src/hoteldata/urls.py
+++ src/hoteldata/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HotelListView
+from .views import CityHotelListView, HotelListView
 
 urlpatterns = [
     path("", HotelListView.as_view()),
+    path("<str:city>/", CityHotelListView.as_view()),
 ]
--- src/hoteldata/views.py
+++ src/hoteldata/views.py
@@ -1,7 +1,16 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
 
-from .models import Hotel
+from .models import City, Hotel
 
 
 class HotelListView(ListView):
     model = Hotel
+
+
+class CityHotelListView(ListView):
+    template_name = "hoteldata/hotel_list.html"
+
+    def get_queryset(self):
+        self.city = get_object_or_404(City, abbreviation=self.kwargs["city"])
+        return Hotel.objects.filter(city=self.city)
