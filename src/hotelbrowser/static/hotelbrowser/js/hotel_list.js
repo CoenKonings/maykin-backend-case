0d52aa99c2c4c132542d24af8a3d96537a43bcb9
Coen Konings <coen.konings@hotmail.com>
2025-07-29 20:57:40 +0200

    async update hotels

--- src/hotelbrowser/static/hotelbrowser/js/hotel_list.js
+++ src/hotelbrowser/static/hotelbrowser/js/hotel_list.js
@@ -12,29 +12,54 @@
     document.getElementById("error").innerText = errorMessage;
   };
 
-  // Show the list of cities that match the user's query.
-  const showCityOptions = (cityOptions) => {
-    const cityDataList = document.getElementById("city-options");
+  // Show the hotels located in a city that matches the user's query.
+  const showHotels = (citiesHotels) => {
+    const hotelElements = citiesHotels.map(city => (
+      city.hotel_set.map(hotelName => {
+        const hotelElement = document.createElement("article");
+        hotelElement.classList.add("hotel-info");
 
-    const cityOptionElements = cityOptions.map(city => {
+        const hotelTitleElement = document.createElement("h2");
+        hotelTitleElement.classList.add("hotel-name");
+        hotelTitleElement.appendChild(document.createTextNode(hotelName))
+        hotelElement.appendChild(hotelTitleElement);
+
+        const hotelLocationElement = document.createElement("div");
+        hotelLocationElement.classList.add("hotel-location");
+        hotelLocationElement.appendChild(document.createTextNode(city.name));
+        hotelElement.appendChild(hotelLocationElement);
+
+        return hotelElement;
+      })
+    )).flat();
+
+    const hotelListElement = document.getElementById("hotel-list");
+    hotelListElement.replaceChildren(...hotelElements);
+  };
+
+  // Show the list of cities that match the user's query.
+  const showCityOptions = (cities) => {
+    const cityOptionElements = cities.map(city => {
       const option = document.createElement("option");
       option.value = city.name;
       option.innerHTML = city.name;
       return option;
     });
 
+    const cityDataList = document.getElementById("city-options");
     cityDataList.replaceChildren(...cityOptionElements);
   };
 
-  // Get and display possible city names as the user types.
-  const handleCityKeyUp = async (cityQuery) => {
-    const response = await fetch("/api/cities/?" + new URLSearchParams({
+  // Get and display possible city names and hotels as the user types.
+  const getCitiesHotels = async (cityQuery) => {
+    const response = await fetch("/api/cities-hotels/?" + new URLSearchParams({
       name: cityQuery
     }).toString());
 
     if (response.status === 200) {
-      const cities = await response.json();
-      showCityOptions(cities);
+      const citiesHotels = await response.json();
+      showCityOptions(citiesHotels);
+      showHotels(citiesHotels);
     } else {
       showHttpError(response.status);
     }
@@ -45,6 +70,9 @@
   document.getElementById("city-name-text-input")
     .addEventListener("keyup", (event) => {
       const cityQuery = event.target.value;
-      handleCityKeyUp(cityQuery);
+      getCitiesHotels(cityQuery);
     });
+
+  // Get city and hotel data when page loads.
+  getCitiesHotels("");
 })();
